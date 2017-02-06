64864afce9ed764ceccc4564d90f852e5c3522c7
Tony Ko <htko89@gmail.com>
2017-02-06 11:20:35 -0500

    Add: Initial turn mechanics

--- release/static/js/index.js
+++ release/static/js/index.js
@@ -4,14 +4,6 @@ init
 *****************************************************************************************************
 *****************************************************************************************************/
 
-var players = {
-  "p1": {
-    "type": "human"
-  },
-  "p2": {
-    "type": "computer"
-  }
-};
 var combo = [
   // rows
   [ 0, 1, 2 ],
@@ -25,8 +17,16 @@ var combo = [
   [ 0, 4, 8 ],
   [ 6, 4, 2 ]
 ];
+var players = {
+  "p1": {
+    "type": "human"
+  },
+  "p2": {
+    "type": "computer"
+  }
+};
 var turn = "p1";
-
+var debug = 0;
 
 /*****************************************************************************************************
 *****************************************************************************************************
@@ -46,12 +46,12 @@ $( document ).ready( function() {
     var data = this.id.split( "-" );
     switch ( data[ 1 ] ) {
       case "human":
-        $( this ).addClass( "is-active" );
         $( "#" + data[ 0 ] + "-computer" ).removeClass( "is-active" );
+        $( "#" + data[ 0 ] + "-human" ).addClass( "is-active" );
         players[ data[ 0 ] ][ "type" ] = "human";
         break;
       case "computer":
-        $( this ).addClass( "is-active" );
+        $( "#" + data[ 0 ] + "-computer" ).addClass( "is-active" );
         $( "#" + data[ 0 ] + "-human" ).removeClass( "is-active" );
         players[ data[ 0 ] ][ "type" ] = "computer";
         break;
@@ -59,22 +59,35 @@ $( document ).ready( function() {
   } );
 
   $( "#dash #select #go" ).click( function() {
-    console.log( players );
     if ( Math.round( Math.random() ) ) {
-      turn = p2;
+      turn = "p2";
     }
     setView( "game" );
-    setTurnMessage();
+    turnAction();
   } );
 
   $( "#game #board .button" ).click( function() {
-    var element = this;
-    $(element + " img").attr( "checked" )
+    if(players[turn]["type"] === "human"){ // only lets you press if your turn is human
+      turnAction();
+    }
   } );
 
-  function setTurnMessage() {
-    $( "#status #message" ).html( ( turn === "p1" ? "Player 1's Turn" : "Player 2's Turn" ) );
+  function turnAction(){
+    setTurnMessage();
+    console.log(turn,players);
+
+
+
+    turn = ( turn === "p1" ? "p2" : "p1");
+    if (players[turn]["type"] === "computer"){
+      // debug++;
+      // if ( debug < 10 ) {
+      //   turnAction();
+      // }
+    }
   }
+
+
 
 } );
 
@@ -87,19 +100,15 @@ UI
 $.fn.extend( {
   showElement: function() {
     var element = this;
-    element.each( function() {
-      $( element ).removeClass( "dead" );
-      $( element ).removeClass( "hidden" );
-    } );
+    $( element ).removeClass( "dead" );
+    $( element ).removeClass( "hidden" );
   },
   hideElement: function() {
     var element = this;
-    element.each( function() {
-      $( element ).addClass( "hidden" );
-    } );
+    $( element ).addClass( "hidden" );
     setTimeout( function() {
       $( element ).addClass( "dead" );
-    }, 500 );
+    }, 800 );
   }
 } );
 
@@ -115,4 +124,8 @@ function setView( view ) {
       $( "#game #board, #dash #score, #status #back" ).showElement();
       break;
   }
+}
+
+function setTurnMessage() {
+  $( "#status #message" ).html( ( turn === "p1" ? "Player 1's Turn" : "Player 2's Turn" ) );
 }
